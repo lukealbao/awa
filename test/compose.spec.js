925e058a9b5184aaf806a34bf00fc585e271935b
Luke Albao <luke.albao@firstperformance.com>
2017-08-08 18:08:26 -0400

    Specifies lib/compose

---
+++ test/compose.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import test from 'ava';
+import compose from '../lib/compose';
+
+test('Composes any number of 1-arity functions', t => {
+  const inc = x => x+1;
+
+  const thrice = compose(inc, inc, inc);
+  const input = 1;
+  
+  t.is(thrice(input), input + 3);
+});
+
+test('Calls functions in left-to-right order', t => {
+  const inc = x => x+1;
+  const double = x => x*2;
+
+  const stack = compose(inc, double);
+  t.is(stack(1), 4);
+});
