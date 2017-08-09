eb5724f5f8b1e094aeb9f26cb0a2d954c51339f0
Luke Albao <luke.albao@firstperformance.com>
2017-08-09 18:46:11 -0400

    Specifies forEach

---
+++ test/forEach.spec.js
@@ -0,0 +1,30 @@
+// @flow
+
+'use strict';
+
+const test = require('ava');
+const forEach = require('../sinks/forEach');
+const sequence = require('../transducers/sequence');
+
+test('Returns a Promise resolving the number of entries processed', t => {
+  t.plan(1);
+
+  return forEach(() => (null), [1, 2, 3])
+    .then(count => {
+      t.is(count, 3);
+    });
+});
+
+test('Processes each input', t => {
+  t.plan(3);
+
+  return forEach(() => t.pass(), [1, 2, 3]);
+});
+
+test('Handles mixed async/sync values', t => {
+  const acc = [];
+  const source = sequence([1, Promise.resolve(2), Promise.resolve(3)]);
+
+  return forEach(n => acc.push(n), source)
+    .then(() => t.deepEqual(acc, [1, 2, 3]));
+});
